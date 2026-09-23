b95760d74e318729ad2a5e61f3b67e89caaf1de7
Joseph Leonor <josephleonor@gmail.com>
2026-09-22 22:26:24 -0400

    Add raco_ecf/__manifest__.py

---
+++ raco_ecf/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "RACO e-CF Laboratory",
+    "version": "18.0.0.1.0",
+    "category": "Accounting/Localizations",
+    "summary": "Laboratorio de comprobantes electrónicos para Odoo 18",
+    "license": "LGPL-3",
+    "depends": ["account", "l10n_do"],
+    "data": ["security/ir.model.access.csv", "views/ecf_views.xml"],
+    "installable": True,
+    "application": False,
+}
